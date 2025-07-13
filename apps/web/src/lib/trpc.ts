27e6afd959e1184ebdd6f0f2dd6ee961e86910e5
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-07-13 21:59:01 +0700

    feat: authenticate trpc

--- apps/web/src/lib/trpc.ts
+++ apps/web/src/lib/trpc.ts
@@ -1,11 +1,16 @@
-import { createTRPCClient, httpBatchLink, loggerLink } from '@trpc/client';
-import type { AppRouter } from '@freshmen68/trpc';
 import { env } from '$env/dynamic/public';
+import type { AppRouter } from '@freshmen68/trpc';
+import { createTRPCClient, httpBatchLink, loggerLink } from '@trpc/client';
 
 export const trpcClient = createTRPCClient<AppRouter>({
   links: [
     loggerLink(),
     httpBatchLink({
+      fetch: (url, options) => fetch(url, {
+        ...options,
+        credentials: "include"
+      }),
       url: env.PUBLIC_TRPC_URL || 'http://localhost:8787/trpc',
-    })],
+    })
+  ],
 });
