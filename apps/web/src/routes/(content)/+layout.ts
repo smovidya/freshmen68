a359c646bf64fc0403a122d9647b44924394c5c8
Patsagorn <patsagorn_y@outlook.com>
2025-07-14 15:33:18 +0700

    fix: handle errors in student info loading and improve error logging

--- apps/web/src/routes/(content)/+layout.ts
+++ apps/web/src/routes/(content)/+layout.ts
@@ -1,13 +1,23 @@
 import type { LayoutLoad } from "./$types";
 import { trpcClient } from "$lib/trpc";
+import { isTRPCClientError } from '@trpc/client';
 
-export const load: LayoutLoad = async ({ fetch }) => {
-  const [student] = await Promise.all([
-    // trpcClient({ fetch }).user.whoami.query(),
-    trpcClient({ fetch }).user.getStudentInfo.query()
-  ]);
-
-  return {
-    student
-  };
+export const load: LayoutLoad = async () => {
+  try {
+    const [student] = await Promise.all([
+      trpcClient().user.getStudentInfo.query()
+    ]);
+    return {
+      student
+    };
+  } catch (error) {
+    if (isTRPCClientError(error)) {
+      console.error("TRPC Client Error:", error.message);
+    } else {
+      console.error("Unexpected Error:", error);
+    }
+    return {
+      student: null
+    };
+  }
 };
