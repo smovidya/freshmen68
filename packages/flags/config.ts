bbde16cb5ba7f73cd2245b671c71cce9f532df4e
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-07-22 18:24:44 +0700

    feat: enable group announcement featuref

--- packages/flags/config.ts
+++ packages/flags/config.ts
@@ -12,7 +12,7 @@ export const features = {
     start: "2025-07-19T12:00:00+07:00",
     end: "2025-07-21T23:59:59+07:00",
   },
-  "group-announcement": false,
+  "group-announcement": true,
   "game-playing": {
     start: "2025-07-26T11:00:00+07:00",
     end: "2025-07-29T23:59:59+07:00",
