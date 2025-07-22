f672c9ba5f93b66623a759c31659005481a8e295
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-07-22 15:49:35 +0700

    add group result page

---
+++ apps/web/src/routes/(content)/group-result/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import BackButton from "$lib/components/back-button.svelte";
+
+</script>
+
+<main class="container mx-auto flex h-full max-w-[60rem] flex-col px-5 py-14">
+	<nav class="flex items-center justify-between gap-4">
+		<BackButton href="/menu" />
+		<div class="text-center">
+			<h1 class="text-3xl font-medium">ประกาศผลกรุ๊ป</h1>
+		</div>
+		<div class="w-10"></div>
+	</nav>
+</main>
+
+dic
