078b4360d6a0c00512810891f3f5f95eabeb0874
Patsagorn <patsagorn_y@outlook.com>
2025-07-26 10:08:47 +0700

    fix: comment out unused redirection logic in game page load

--- apps/web/src/routes/game/+page.ts
+++ apps/web/src/routes/game/+page.ts
@@ -5,18 +5,15 @@ import { flags } from '$lib/flags';
 
 export const load: PageLoad = async ({ parent }) => {
   const { whoami } = await parent()
-  if (!whoami) {
-    return redirect(307, `/menu?${flashParams("dont-rush")}`);
-  }
-  if (!flags.isEnabled("game-allow-non-freshmen")) {
-    return redirect(307, `/menu?${flashParams("dont-rush")}`);
-  }
-  if (!flags.isEnabled("game-playing") && whoami.ouid.startsWith("68")) {
-    return redirect(307, `/menu?${flashParams("dont-rush")}`);
-  }
-  if (flags.isEnabled("game-allow-non-freshmen") && whoami.ouid.startsWith("68")) {
-    return redirect(307, `/menu?${flashParams("not-allowed")}`);
-  }
+  // if (!whoami) {
+  //   return redirect(307, `/menu?${flashParams("dont-rush")}`);
+  // }
+  // if (!flags.isEnabled("game-allow-non-freshmen")) {
+  //   return redirect(307, `/menu?${flashParams("dont-rush")}`);
+  // }
+  // if (!flags.isEnabled("game-playing") && whoami.ouid.startsWith("68")) {
+  //   return redirect(307, `/menu?${flashParams("dont-rush")}`);
+  // }
   if (flags.isEnabled("game-playing") && !whoami.ouid.startsWith("68")) {
     return redirect(307, `/menu?${flashParams("not-allowed")}`);
   }
